8ac81c42b742bc694f83d85ca3a9a09e8f5d0115
Paul Evans <decretist@gmail.com>
2021-04-12 11:40:03 -0700

    Add stylo bi- and tri-gram analysis

---
+++ ngrams.R
@@ -0,0 +1,38 @@
+#
+# Paul Evans (10evans@cua.edu)
+# 12 April 2021)
+#
+setwd("~/Work/Ngrams")
+library(stylo)
+
+stylo.results = stylo(
+  gui = TRUE,
+  corpus.lang = "Latin.corr",
+  analyzed.features = "c",
+  ngram.size = 2,
+  mfw.min = 80, mfw.max = 80,
+  analysis.type = "PCR",
+  sampling = "normal.sampling",
+  sample.size = 3600,
+  write.jpg.file = TRUE,
+  custom.graph.title = "Bigrams",
+  custom.graph.filename = "JPGs/bigrams",
+)
+# summary(stylo.results)
+print(stylo.results$features.actually.used)
+
+stylo.results = stylo(
+  gui = TRUE,
+  corpus.lang = "Latin.corr",
+  analyzed.features = "c",
+  ngram.size = 3,
+  mfw.min = 80, mfw.max = 80,
+  analysis.type = "PCR",
+  sampling = "normal.sampling",
+  sample.size = 3600,
+  write.jpg.file = TRUE,
+  custom.graph.title = "Trigrams",
+  custom.graph.filename = "JPGs/trigrams",
+)
+# summary(stylo.results)
+print(stylo.results$features.actually.used)
